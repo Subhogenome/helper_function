5701b85b40aca328ff7602ab16cdb4d6917fa2f8
Subhogenome <chatterjeesubhodeep08@gmail.com>
2024-01-04 12:14:51 +0530

    first commit

--- helper_function.py
+++ helper_function.py
@@ -3,3 +3,12 @@ import pandas as pd
 def keep_first_two_words(sentence):
     words = sentence.split()
     return ' '.join(words[:2])
+
+
+def pattern_filter(meta_new,organism,master_db):
+ df =pd.DataFrame()
+
+ for i in range(meta_new.shape[0]):
+    d=d[d['Compound Name'].str.contains((meta_new["Name"][i]),case=False) ]
+    df=pd.concat([d,df])
+ return df
